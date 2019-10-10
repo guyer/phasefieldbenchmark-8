75453b70a63c5375888751200d10a9d3c6043d00
Jonathan Guyer <guyer@nist.gov>
2019-10-10 16:46:36 -0400

    Add slurm driver

---
+++ problem8b.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --partition rack4,rack5,rack6 # -p, partition
+#SBATCH --time 12:00:00               # -t, time (hh:mm:ss or dd-hh:mm:ss)
+#SBATCH --nodes=1                     # -N, total number of machines
+#SBATCH --ntasks=64                   # -n, 64 MPI ranks per Opteron machine
+#SBATCH -J Problem-8b
+#SBATCH -D /data/$USER/CHiMaD/phase_field/phasefieldbenchmark-8
+
+conda activate fipy
+
+export OMP_NUM_THREADS=1
+mpiexec -n 1 smt run -n $SLURM_NTASKS --main benchmark8b.py params8b.yaml "$@"
