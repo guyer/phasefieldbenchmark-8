0dd391a5c01c037c99075cf4c1665d76fb4bb035
Jonathan Guyer <guyer@nist.gov>
2020-02-02 17:43:13 -0500

    Differentiate between different output revisions

--- tools/plot.py
+++ tools/plot.py
@@ -4,28 +4,51 @@ import fipy as fp
 
 __all__ = ['plot_avrami', 'plot_energy', 'plot_fraction', 'plot_phi']
 
-def load_stats(record):
+def load_stats_r1(record):
     """Load stats.txt TSV file associated with `record`
-    
+
+    Loads results from r1 benchmark
+
     Returns
     ------
     time, fraction, energy : tuple of array
     """
     fname = "Data/{}/stats.txt".format(record["label"])
     return fp.numerix.loadtxt(fname, unpack=True)
-    
-def plot_avrami(record):
-    tt, fraction, energy = load_stats(record)
+
+def load_stats_r2(record):
+    """Load stats.txt TSV file associated with `record`
+
+    Loads results from r2 benchmark
+
+    Returns
+    ------
+    time, fraction, particle_count, energy : tuple of array
+    """
+    fname = "Data/{}/stats.txt".format(record["label"])
+    return fp.numerix.loadtxt(fname, skiprows=1, unpack=True)
+
+def load_stats(record, rev):
+    if rev == 1:
+        tt, fraction, energy = load_stats_r1(record)
+        particle_count = None
+    elif rev == 2:
+        tt, fraction, particle_count, energy = load_stats_r2(record)
+
+    return tt, fraction, particle_count, energy
+
+def plot_avrami(record, rev=1):
+    tt, fraction, particle_count, energy = load_stats(record, rev=rev)
     plt.loglog(tt, -fp.tools.log10(1-fraction),
                label="$\Delta t = {}$".format(record["--dt"]))
 
-def plot_energy(record):
-    tt, fraction, energy = load_stats(record)
+def plot_energy(record, rev=1):
+    tt, fraction, particle_count, energy = load_stats(record, rev=rev)
     plt.plot(tt, energy,
              label="$\Delta t = {}$".format(record["--dt"]))
 
-def plot_fraction(record):
-    tt, fraction, energy = load_stats(record)
+def plot_fraction(record, rev=1):
+    tt, fraction, particle_count, energy = load_stats(record, rev=rev)
     plt.plot(tt, fraction,
              label="$\Delta f = {} \Delta f_0$".format(record["--factor"]))
 
